22146ec0b6b072b87a35ea053ceef87f5c9e55b1
matin <matin912nezami@gmail.com>
2023-12-02 15:00:13 -0800

    upload 2

---
+++ 2/2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	char chr;
+	cout << "Enter charcter: ";
+	cin >> chr;
+	
+	switch (chr) {
+		case 'a':
+			cout << "VOWEL";
+			break;
+			
+		case 'u':
+			cout << "VOWEL";
+			break;
+			
+		case 'o':
+			cout << "VOWEL";
+			break;
+			
+		case 'i':
+			cout << "VOWEL";
+			break;
+			
+		case 'e':
+			cout << "VOWEL";
+	}
+	
+}
